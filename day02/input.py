0c1065a5856c562b58cfb8579350e691d6c46cd8
shensjy <shensjy@126.com>
2020-03-07 14:31:48 +0800

    version1

--- day02/input.py
+++
@@ -1,6 +0,0 @@
-#输入语句
-#使用方式
-#help(input)
-
-num = input("Number: ")
-num + 10
--- day02/print.py
+++
@@ -1,7 +0,0 @@
-#输出语句
-#使用方式
-print('Hello World!')
-print('Hello'+'World!')
-print('Hello', 'World!')
-print('Hello','World!',sep='***')
-print('Hello','World@',sep='***', end='')
